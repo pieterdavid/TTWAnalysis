395acc370c6d503ab9f8e59b0c3d5acd564537e8
Pieter David <pieter.david@cern.ch>
2016-06-15 10:01:10 +0200

    IFDEF include of NewTypes (for dictionary

--- src/classes.h
+++ src/classes.h
@@ -2,7 +2,11 @@
 #include <vector>
 #include <utility>
 
+#ifndef TTW_PLOTTER
 #include "cp3_llbb/TTWAnalysis/interface/NewTypes.h"
+#else
+#include "cp3_llbb/TTWAnalysis/interface/Types.h"
+#endif
 
 namespace TTWAnalysis {
   struct dictionary {
@@ -14,7 +18,9 @@ namespace TTWAnalysis {
     TTWAnalysis::GenParticle dummy22;
     std::vector<TTWAnalysis::GenParticle> dummy23;
     // new types
+#ifndef TTW_PLOTTER
     TTWAnalysis::Lepton dummy24;
     TTWAnalysis::DiLepton dummy25;
+#endif
   };
 }
