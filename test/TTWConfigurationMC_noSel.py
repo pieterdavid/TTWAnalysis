c964d44e79c4e4b25e7bc7b82efd3dfe95afaf29
Pieter David <pieter.david@cern.ch>
2017-03-13 17:11:07 +0100

    use sync file for test

--- test/TTWConfigurationMC_noSel.py
+++ test/TTWConfigurationMC_noSel.py
@@ -39,7 +39,7 @@ process = framework.create()
 #     '/store/mc/RunIIFall15MiniAODv2/TTWJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-madspin-pythia8/MINIAODSIM/PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1/60000/14C51DB0-D6B8-E511-8D9B-8CDCD4A9A484.root'
 #     )
 process.source.fileNames = cms.untracked.vstring(
-    "/store/mc/RunIISummer16MiniAODv2/TTWJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-madspin-pythia8/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext1-v3/70000/569A7690-7FC8-E611-A462-FA163EDE7C8D.root"
+    "/store/mc/RunIISummer16MiniAODv2/TTWJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-madspin-pythia8/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext2-v1/110000/0015BB42-9BAA-E611-8C7F-0CC47A7E0196.root" ## ttH-multilepton sync sample
     )
 
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(1000))
